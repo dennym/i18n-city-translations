de21d87fc7b008c14a5788db901bd360159a75cb
Denny Mueller <denny@dennymueller.de>
2017-09-26 14:07:41 +0200

    add links as reminder

--- lib/i18n_city_translations/translate_city.rb
+++ lib/i18n_city_translations/translate_city.rb
@@ -2,6 +2,9 @@ require 'yaml'
 require 'open-uri'
 require 'nokogiri'
 
+# http://api.geonames.org/search?name=moskva&country=RU&featureCode=PPLC&maxRows=10&type=rdf&username=demo
+# http://download.geonames.org/export/dump/alternateNames.zip
+
 module I18nCityTranslations
   class TranslateCity
     attr_accessor :output_dir
